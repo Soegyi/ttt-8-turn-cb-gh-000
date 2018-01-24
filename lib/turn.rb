01d39e3cf4e0a96f51a9f0c83d4a2d72d8eca953
Soe Thu Hein <killerseven007@gmail.com>
2018-01-24 14:06:22 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -13,3 +13,21 @@ end
 def move(array,index,value="X")
   array[index] = value
 end
+def valid_move?(board,index)
+
+  if (index <= 8 && index >= 0) && (position_taken?(board,index) == false)
+  return true
+  
+  else return false
+  
+  end
+
+
+end
+
+def position_taken?(board,index)
+  if(board[index] == " " || board[index] == "" || board[index] == nil)
+     return false
+  else return true   
+  end
+end
