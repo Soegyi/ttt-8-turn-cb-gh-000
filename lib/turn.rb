a5d991e0f90c5aa213af27dca2fa5614e4358a50
Soe Thu Hein <killerseven007@gmail.com>
2018-01-24 14:45:08 +0000

    automatically backed up by learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -3,7 +3,8 @@ def turn(board)
  input = gets.strip
  index = input_to_index(input)
  if valid_move?(board,index)
-  
+    move(board,index,value="X")
+    display_board(board)
  else 
    turn(board)
 end
